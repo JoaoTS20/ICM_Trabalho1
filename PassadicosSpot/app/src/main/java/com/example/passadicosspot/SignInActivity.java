bf4bfbc3af712ed451f2721764f3b740038f5efa
João Soares <joaots@ua.pt>
2020-11-22 23:44:43 +0000

    MainActivity_Navigation.java Done

--- PassadicosSpot/app/src/main/java/com/example/passadicosspot/SignInActivity.java
+++ PassadicosSpot/app/src/main/java/com/example/passadicosspot/SignInActivity.java
@@ -102,7 +102,7 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
                             Toast.makeText(SignInActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         } else {
-                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                            startActivity(new Intent(SignInActivity.this, MainActivity_Navigation.class));
                             finish();
                         }
                     }
